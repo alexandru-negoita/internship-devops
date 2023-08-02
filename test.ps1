96a58a745c465cb9b8c568a80304d6b87b474196
unknown <alexandru.negoita@wirtek.com>
2023-08-02 19:21:56 +0300

    added request

--- test.ps1
+++ test.ps1
@@ -1,1 +1,14 @@
 Write-Output "test"
+
+for (($i = 0); $i -lt 10; $i++) {
+    if ($i % 2 -eq 0) {
+        $request = Invoke-WebRequest "https://api.open-meteo.com/v1/forecast?latitude=$(50+$i)&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
+
+        $yml = $request.Content | ConvertFrom-Json | ConvertTo-Yaml | Out-File "C:\DevOps-Tasks\Day2T\internship-devops\output$i.yaml"
+        Write-Output $yml
+    }
+    
+}
+
+
+# Write-Output $request.Content
